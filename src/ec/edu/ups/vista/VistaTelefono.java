f15a33c3b97b70fab4e0c147f240648773bc40db
mtoledot1 <mtoledot1@est.ups.edu.ec>
2020-05-12 21:20:16 -0500

    Creacion de las clases Vista

--- src/ec/edu/ups/vista/VistaTelefono.java
+++ src/ec/edu/ups/vista/VistaTelefono.java
@@ -5,10 +5,67 @@
  */
 package ec.edu.ups.vista;
 
+import ec.edu.ups.modelo.Telefono;
+import java.util.List;
+import java.util.Scanner;
 /**
  *
  * @author tano
  */
 public class VistaTelefono {
+    private Scanner leer;
     
+    public VistaTelefono(){
+        leer = new Scanner(System.in);
+    }
+    
+    public Telefono ingresarTelefono(){
+        System.out.println("Ingrese los datos del telefono:");
+        System.out.print("Codigo: ");
+        int codigo = leer.nextInt();
+        leer.nextLine();
+        System.out.print("Numero: ");
+        String numero = leer.nextLine();
+        System.out.print("Tipo: ");
+        String tipo = leer.nextLine();
+        System.out.print("Operadora: ");
+        String operadora = leer.nextLine();
+        return new Telefono(codigo, numero, tipo, operadora);
+    }
+    
+    public Telefono actualizarTelefono(){
+        System.out.print("Ingrese el codigo del telefono: ");
+        int codigo = leer.nextInt();
+        System.out.println("Ingrese los nuevos datos");
+        leer.nextLine();
+        System.out.print("Numero: ");
+        String numero = leer.nextLine();
+        System.out.print("Tipo: ");
+        String tipo = leer.nextLine();
+        System.out.print("Operadora: ");
+        String operadora = leer.nextLine();
+        return new Telefono(codigo, numero, tipo, operadora);
+    }
+    
+    public Telefono eliminarTelefono(){
+        System.out.print("Ingrese el codigo del telefono: ");
+        int codigo = leer.nextInt();
+        return new Telefono(codigo, null, null, null);
+    }
+    
+    public int buscarTelefono(){
+        System.out.print("Ingrese el codigo del telefono: ");
+        int codigo = leer.nextInt();
+        return codigo;
+    }
+    
+    public void verTelefono(Telefono telefono){
+        System.out.println("Datos del Telefono: " + telefono.toString());
+    }
+    
+    public void verTelefonos(List<Telefono> telefonos){
+        telefonos.forEach((telefono) -> {
+            System.out.println("Datos del Telefono: " + telefono.toString());
+        });
+    }
 }
